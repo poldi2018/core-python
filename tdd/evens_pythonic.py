19f1f8f4bd40d392d1bef7d87f4f680f80c140fe
Peer Naupold <greenion@gmx.de>
2019-08-12 23:33:33 +0200

    refactoring evens_pythonic.py

--- tdd/evens_pythonic.py
+++ tdd/evens_pythonic.py
@@ -2,14 +2,9 @@ def is_even(number):
     return number % 2 == 0
 
 def even_number_of_evens(numbers):
-    evens = 0
-    for n in numbers:
-        if is_even(n):
-            evens += 1
-    if evens == 0:
-        return False
-    else:
-        return is_even(evens)
+    evens = sum([1 for n in numbers if is_even(n)])
+    return False if evens == 0 else is_even(evens)
+
 
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2]) == False, "One even number"
